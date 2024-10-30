8248c03e58d8fb4529e2accb992bde4ffbcd15f5
giblim <giblimdev@gmail.com>
2024-10-30 17:13:52 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+export const routes: Routes = [
+   { path: 'products/1', component: ProductDetailComponent},
+   { path: 'login', component: LoginComponent}, 
+   { path: 'cart', component: CartComponent},
+   { path: 'order', component: OrderComponent},
+   {path: '', component: HomeComponent}
 
-export const routes: Routes = [];
+];
